dcc8f5552681855e651225d90fec851ab292b868
Nicolas-Yoles <nicoyoles@gmail.com>
2022-06-20 13:46:04 -0300

    commit 01 pymes front end app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from "@angular/common/http"
+import {RouterModule} from "@angular/router";
+import {APP_BASE_HREF} from "@angular/common"
+import { ReactiveFormsModule } from "@angular/forms";
 
 import { AppComponent } from './app.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { ArticulosFamiliasComponent } from './components/articulos-familias/articulos-familias.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { ArticulosComponent } from './components/articulos/articulos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    ArticulosFamiliasComponent,
+    MenuComponent,
+    ArticulosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      {path: "", redirectTo: '/inicio', pathMatch: 'full'},
+      {path: 'inicio', component: InicioComponent},
+      {path: 'articulosfamilias', component: ArticulosFamiliasComponent},
+      {path: 'articulos', component: ArticulosComponent}
+    ])
   ],
-  providers: [],
+  providers: [
+    {provide:APP_BASE_HREF,useValue:"/"}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/models/articulo-familia.ts
@@ -0,0 +1,15 @@
+export interface ArticuloFamilia {
+    IdArticuloFamilia: number;
+    Nombre: string;
+}
+
+export const ArticulosFamilias: ArticuloFamilia[]=[
+    { IdArticuloFamilia: 1, Nombre: "Monitores"},
+    { IdArticuloFamilia: 2, Nombre: "Teclados"},
+    { IdArticuloFamilia: 3, Nombre: "Ratones"},
+    { IdArticuloFamilia: 4, Nombre: "Parlantes"},
+    { IdArticuloFamilia: 5, Nombre: "Headsets"},
+    { IdArticuloFamilia: 6, Nombre: "Joysticks"},
+    { IdArticuloFamilia: 7, Nombre: "Gabinetes"},
+    { IdArticuloFamilia: 8, Nombre: "Procesadores"}
+]
